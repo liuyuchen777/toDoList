96e4b36e7a962ed0a7a4dd60a723e84a22e9a5ff
liuyuchen777 <liuyuchen.hust@gmail.com>
2021-04-02 10:55:25 +0900

    add backend

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
  * @Author: Liu Yuchen
  * @Date: 2021-04-02 22:06:14
  * @LastEditors: Liu Yuchen
- * @LastEditTime: 2021-04-02 10:43:14
+ * @LastEditTime: 2021-04-02 10:54:18
  * @Description:
  * @FilePath: /Local_Lab/toDoList/main.go
  * @GitHub: https://github.com/liuyuchen777
@@ -14,9 +14,27 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
 func main() {
+	// create database
+	// sql: CREATE DATABASE todo;
+	// link db
+	db, err := gorm.Open("mysql", "root:lyc7758321321@(localhost:3306)/todo?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// gin
 	r := gin.Default()
 
 	// load static files
@@ -28,11 +46,31 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	// 待办事项
-	// 添加
-	// 查看
-	// 修改
-	// 删除
+	// v1 group
+	v1Group := r.Group("v1")
+	{
+		// 待办事项
+		// 添加
+		v1Group.POST("/todo", func(c *gin.Context) {
+
+		})
+		// 查看所有的待办事项
+		v1Group.GET("/todo", func(c *gin.Context) {
+
+		})
+		// 查看某一个待办事项
+		v1Group.GET("/todo/:id", func(c *gin.Context) {
+
+		})
+		// 修改
+		v1Group.PUT("/todo/:id", func(c *gin.Context) {
+
+		})
+		// 删除
+		v1Group.DELETE("/todo/:id", func(c *gin.Context) {
+
+		})
+	}
 
 	r.Run(":9090")
 }
